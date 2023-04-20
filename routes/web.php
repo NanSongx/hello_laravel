1406ffdd41297a691c91c77e0bc79253fbe850f8
xiangnan.song <nansongx@gmail.com>
2023-04-20 13:23:34 +0800

    发送找回密码链接

--- routes/web.php
+++ routes/web.php
@@ -33,5 +33,15 @@ Route::delete('logout', 'SessionsController@destroy')->name('logout');
 
 Route::get('signup/confirm/{token}', 'UsersController@confirmEmail')->name('confirm_email');
 
+// 忘记密码
+//  填写 Email 的表单
+Route::get('password/reset',  'PasswordController@showLinkRequestForm')->name('password.request');
+// 处理表单提交，成功的话就发送邮件，附带 Token 的链接
+Route::post('password/email',  'PasswordController@sendResetLinkEmail')->name('password.email');
+// 显示更新密码的表单，包含 token
+Route::get('password/reset/{token}',  'PasswordController@showResetForm')->name('password.reset');
+// 对提交过来的 token 和 email 数据进行配对，正确的话更新密码
+Route::post('password/reset',  'PasswordController@reset')->name('password.update');
+
 
 
